2fa23ecf6656b8f2a58f761dac9d26c87195339c
Morten Piibeleht <morten.piibeleht@gmail.com>
2021-09-27 20:45:13 +1300

    Use at-__DIR__ instead of dirname(at-__FILE__)

--- src/GRASP.jl
+++ src/GRASP.jl
@@ -12,12 +12,12 @@ using HalfIntegers: HalfInteger, HalfInt
 # Includes and imports related to compiled Fortran code.
 # ------------------------------------------------------------------------------------------
 
-let grasp_path_file = joinpath(dirname(@__FILE__), "../deps/grasp-path.jl")
+let grasp_path_file = joinpath(@__DIR__, "../deps/grasp-path.jl")
     isfile(grasp_path_file) || error("deps/grasp-path.jl does not exist. Run `Pkg.build(\"GRASP\")`.")
     include(grasp_path_file)
 end
 
-const libgrasp_so = joinpath(dirname(@__FILE__), "..", "deps", "libgrasp.so")
+const libgrasp_so = joinpath(@__DIR__, "..", "deps", "libgrasp.so")
 isfile(libgrasp_so) || error("$(libgrasp_so) does not exist. Run `Pkg.build(\"GRASP\")`.")
 
 #
